1e7f507387b27027119a0721d9c0f77986d7e62a
Nikhil Yadav <facts.foundr@gmail.com>
2023-10-28 22:41:02 +0530

    updated text size/color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,14 +24,14 @@ const Header = () => {
                                         <h2 className='md:text-[90px] text-[60px]  mb-[-20px] font-bold' >TATSAM</h2>
                                         <h2 className='md:text-[30px]  text-[20px] pt-2 text-slate-200'>The Hindi Society of NSUT</h2>
                                         
-                                        <div className='md:text-sm leading-3 bg-black p-2 bg-opacity-20 rounded text-[12px] text-center  tracking-wide  text-slate-200'>
+                                        <div className='md:text-[17px] md:leading-[22px] leading-[16px] bg-black p-2 bg-opacity-20 rounded text-[13px] text-center tracking-wide  text-slate-300'>
                                         Welcome to Tatsam - The Hindi Society of NSUT, where ordinary moments transform into extraordinary memories, and dreams find their wings to soar.
-                                        <p className='hidden md:inline text-slate-200 tracking-wide '>
+                                        <p className='hidden md:inline text-slate-300 tracking-wide '>
                                         At Tatsam, we're not just a college society, we're a community of dreamers, creators, and visionaries. We aim to ignite the spark of passion,
                                          creativity, and culture through the beautiful tapestry of Hindi language and art.
                                         </p>
                                         </div>
-                                        <button className=' drop-shadow-xl  duration-300 rounded-lg bg-[#FF9100] hover:bg-white/40 text-white md:px-6 md:py- md:text-[18px] mt-6 text-[14px]  mb-2 px-4 py-1 '>
+                                        <button className=' drop-shadow-xl  duration-300 rounded-lg bg-[#FF9100] hover:bg-white/40 text-white md:px-6 md:py- md:text-[18px] mt-6 text-[14px]  mb-2 px-5 py-2 '>
                                                  Join Us
                                         </button>
 
