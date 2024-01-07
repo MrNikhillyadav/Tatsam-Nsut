701e536751205c25971d0aa3dcd1e06a656987a3
Nikhil Yadav <facts.foundr@gmail.com>
2024-01-07 21:44:34 +0530

    updated form ui

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,24 +3,24 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-        <div className="md:px-20 md:pb-20 px-5 pb-10 flex flex-col overflow-x-hidden">
+        <div className="md:px-20 md:pb-20  px-5  my-24 pb-10 flex flex-col overflow-x-hidden">
             <div>
-                <h1 className="w-full text-center text-[30px] md:text-[45px] mb-5 font-bold text-orange-500">
-                    Contact Us !
+                <h1 className="w-full text-center text-[30px] font md:text-[35px]  my-5 font-bold text-[#f1af33]">
+                    Contact Us
                     <div className="underline ml-auto mr-auto"></div>
                 </h1>
             </div>
 
-            <div className="flex  mr-auto ml-auto gap-20 shadow-lg shadow-indigo-500/40 rounded-lg">
-                <div className="px-5 md:px-10 py-5 flex flex-col gap-5 mt-2">
-                  <h3 className="text-[22px] md:text-[26x] text-center">Have a Problem ? Message Us</h3>
+            <div className="flex border  border-slate-300 shadow-md mt-8 mr-auto ml-auto gap-20  rounded-2xl">
+                <div className="px-5 md:px-12 py-5 flex flex-col gap-5 mt-2">
+                  <h3 className=" text-sm  font-normal text-slate-400  text-center">Have a Problem ? Message Us</h3>
                     <div className="flex md:gap-5 flex-col md:flex-row gap-5">
-                    <input className='outline-none bg-[#f5f5f5] w-[100%] md:w-[50%] p-[8px] md:p-[12px]' type="text" name="" placeholder='Enter Name' id="" />
-                    <input className='bg-[#f5f5f5] outline-none  md:w-[50%] w-[100%] p-[8px] md:p-[12px]' type="email" name="" placeholder='Email' id="" />
+                    <input className='border border-slate-300 rounded-lg   w-[100%] md:w-[50%] p-[8px] md:px-[12px]' type="text" name="" placeholder='Enter Name' id="" />
+                    <input className='border border-slate-300 rounded-lg   md:w-[50%] w-[100%] p-[8px] md:px-[12px]' type="email" name="" placeholder='Email' id="" />
                     </div>
-                    <input className='bg-[#f5f5f5] p-[8px] md:p-[12px] outline-none' type="text" name="" placeholder='Subject' id="" />
-                    <textarea className="bg-[#f5f5f5] outline-none p-[8px] md:p-[12px] "placeholder='Message' name="" id="" cols="30" rows="5"></textarea>
-                    <button className="py-2 duration-1000 outline-none shadow-lg shadow-indigo-500/40  w-[120px] ml-auto mr-auto rounded-xl text-xl text-white font-semi-bold font-serif  bg-orange-500"><a href="#">Submit</a></button>
+                    <input className=' p-[8px] md:px-[12px] border border-slate-300 rounded-lg ' type="text" name="" placeholder='Subject' id="" />
+                    <textarea className="border border-slate-300 rounded-lg  p-[8px] md:px-[12px] "placeholder='Message' name="" id="" cols="30" rows="5"></textarea>
+                    <button className="md:py-1 duration-1000    w-[120px] m-auto rounded-xl text-xl text-white bg-[#f1af33] shadow-md"><a className='text-sm ' href="#">Submit</a></button>
                 </div>
             </div>
         </div>
